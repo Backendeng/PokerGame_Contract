4ad7aefd7959578b6e95465676cc1c0222f2e433
Backendeng <backendeng0417@gmail.com>
2022-10-13 12:01:30 -0400

    Update PokerGame
    Add library

---
+++ PokerGame.sol
@@ -0,0 +1,5 @@
+pragma solidity >=0.8.9 <0.9.0;
+import "@openzeppelin/contracts/access/Ownable.sol";   //for usein onlyOwner
+import "@openzeppelin/contracts/utils/math/SafeMath.sol"; // for a=a+b, a.add(b). why do I use. because security
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol"; // for GET NFT
+
